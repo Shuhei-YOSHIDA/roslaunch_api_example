b9feb9e0f900a46758073ec758274d4e37951275
Shuhei-YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2021-12-16 16:21:08 +0900

    Fixed

--- scripts/roslaunch_api_example.py
+++ scripts/roslaunch_api_example.py
@@ -11,7 +11,7 @@ import time
 if __name__ == "__main__":
     rospy.init_node('roslaunch_api_example')
 
-    print("Example to execute a node")
+    print("Start an example to execute a node")
     package = 'roslaunch_api_example'
     executable = 'roslaunch_api_example_node'
     node = roslaunch.core.Node(package, executable, output="screen")
@@ -19,23 +19,49 @@ if __name__ == "__main__":
     launch = roslaunch.scriptapi.ROSLaunch()
     launch.start()  # Necessary before process is prepared
 
-    process = launch.launch(node)  # Startthe the node
+    process = launch.launch(node)  # Start the the node
 
     print("process is alive? : ", process.is_alive())
 
-    time.sleep(10)
+    time.sleep(5)
 
     print("Stop node process from script")
     process.stop()
     print("process is alive? : ", process.is_alive())
 
     # restart of same node
-    print("restart")
+    print("Respawn the same node")
     process = launch.launch(node)
     print("process is alive? : ", process.is_alive())
 
-    time.sleep(10)
+    time.sleep(5)
 
     print("Stop node process from script")
     process.stop()
     launch.stop()
+    print("Finished an example to execute a node")
+
+    # Execute a launch file
+    print("Start an example to execute a launch file")
+    rospack = rospkg.RosPack()
+    pkg_path = rospack.get_path("roslaunch_api_example")
+    launch_path = pkg_path + '/launch/roslaunch_api_example.launch'
+
+    uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+    roslaunch.configure_logging(uuid)
+    launch = roslaunch.parent.ROSLaunchParent(uuid, [launch_path])
+
+    print("Prepared a launch file")
+    launch.start()
+
+    time.sleep(5)
+
+    launch.shutdown()
+    print("Stop and re-launch the file")
+    launch = roslaunch.parent.ROSLaunchParent(uuid, [launch_path])  # Necessary
+    launch.start()
+
+    time.sleep(5)
+
+    launch.shutdown()
+    print("Finished example of a launch file ")
